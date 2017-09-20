fe842d01ab7238ec059313076c81e67b2992c6fa
Juan David Rodríguez Cuervo <juan.7655@hotmail.com>
2017-09-19 19:33:52 -0500

    Add files via upload

---
+++ gradient_descent.py
@@ -0,0 +1,42 @@
+def run():
+	with open("results-20170918-230306.csv") as file:
+		data = file.readlines()
+
+	m_current = 0
+	b_current = 0
+	final_error = 0
+
+	for i in range(600):
+		[b_current, m_current, final_error] = step_gradient(b_current, m_current, data, 0.0001)
+		print("m:" + str(m_current) + "	|	b:" + str(b_current))
+	print("Error: " + str(final_error))
+
+
+
+
+def step_gradient(b_current, m_current, points, learning_rate):
+	m_gradient = 0
+	b_gradient = 0
+	N = float(len(points) - 1)
+	condition = False
+	error = 0
+
+	print("m:" + str(m_current) + "	|	b:" + str(b_current))
+	for line in points:
+		if condition:
+				text = line.split(",")
+				x = int(text[1])
+				y = int(text[2])
+				function = (y - m_current * x - b_current)/N
+				error += (function**2) * N
+				m_gradient -= 2 * x * function
+				b_gradient -= 2 * function
+		else:
+				condition = True
+	new_m = m_current - (learning_rate * m_gradient)
+	new_b = b_current - (learning_rate * b_gradient)
+
+	return [new_b, new_m, error]
+
+if __name__ == '__main__':
+    run()
