eb1200cef93ab727ae2859b33b292228f6eff35f
juandavid <juan.7655@hotmail.com>
2017-12-08 10:59:19 -0500

    included pandas library

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,16 +1,20 @@
 import time
 import pandas as pd
 
+x_column = "weight (n)"
+y_column = "mpg (n)"
+iterations = 60000
+
 
 def run():
 	data = pd.read_csv("mpg.csv")
 
 	start_time = time.time()
-	m_current = (data["mpg (n)"].mean()-data["mpg (n)"][0])/(data["weight (n)"].mean()-data["weight (n)"][0])
-	b_current = data["mpg (n)"][0]-m_current*data["weight (n)"][0]
+	m_current = (data[y_column].mean()-data[y_column][0])/(data[x_column].mean()-data[x_column][0])
+	b_current = data[y_column][0]-m_current*data[x_column][0]
 	final_error = 0
 
-	for i in range(60000):
+	for i in range(iterations):
 		[b_current, m_current, final_error] = step_gradient(b_current, m_current, data, 0.00000010405)
 		print("m:" + str(m_current) + "	b:" + str(b_current) + "  E:" + str(final_error))
 	print("Error: " + str(final_error))
@@ -18,13 +22,13 @@ def run():
 
 
 def step_gradient(b_current, m_current, points, learning_rate):
-	N = float(len(points) - 1)
+	num_points = float(len(points) - 1)
 	temp_points = points.copy()
 
-	delta = (temp_points["mpg (n)"]-m_current*temp_points["weight (n)"]-b_current)
-	error = (delta**2).sum() / N
-	m_gradient = -(2/N) * (temp_points["weight (n)"] * delta).sum()
-	b_gradient = -(2/N) * delta.sum()
+	delta = (temp_points[y_column]-m_current*temp_points[x_column]-b_current)
+	error = (delta**2).sum() / num_points
+	m_gradient = -(2/num_points) * (temp_points[x_column] * delta).sum()
+	b_gradient = -(2/num_points) * delta.sum()
 
 	new_m = m_current - (learning_rate * m_gradient)
 	new_b = b_current - (learning_rate * b_gradient)
